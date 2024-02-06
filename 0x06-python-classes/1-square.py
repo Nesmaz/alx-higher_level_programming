dcb64c241ad5dc2c95e63462cb7faa5263004324
Nesmaz <nesmabdelazim@gmail.com>
2024-02-06 12:29:30 -0800

    A class Square that defines a square by: (based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """
+    This is square class. It has one method: __init__.
+    """
+
+
+    def __init__(self, size) -> None:
+        """
+        Initializes class attributes
+
+        Args:
+        size: size of square
+        """
+
+
+        self.__size = size
