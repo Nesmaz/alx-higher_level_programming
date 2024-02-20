0eb7a5e399845be78bcc0adc105cac59fa5509ef
Nesmaz <nesmabdelazim@gmail.com>
2024-02-19 18:44:34 -0800

    small fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 '''Module to check if object is an instance of a class that inherited from'''
 
 
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     Returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False.
